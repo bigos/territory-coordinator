b21b02a6966bbf4aa221a2f260ac77b5a65676db
Jacek Podkanski <ruby.object@googlemail.com>
2017-11-07 19:07:40 +0000

    renamed the project

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -1,4 +1,4 @@
-(ns clojure-getting-started.web
+(ns territory-coordinator.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
